4f87f31a32c626ab3febb66c9aa6d3d9b52e7bac
sungeunbyeol <87050900+sungeunbyeol@users.noreply.github.com>
2021-07-30 14:16:38 +0900

    Update Car.java

--- javaStudy/Car.java
+++ javaStudy/Car.java
@@ -1,6 +1,19 @@
 package javaStudy;
 
 public class Car {
-    String name;
-    int number;
+    String name; // 객체변수
+    int number; // 객체변수
+    
+    public Car(){
+        
+    } 
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public Car(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
 }
