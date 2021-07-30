fb42b2003297f765b0ba863aee5ab66f4ff2d73b
sungeunbyeol <87050900+sungeunbyeol@users.noreply.github.com>
2021-07-30 10:35:24 +0900

    Create StringMethodExam.java
    String

---
+++ javaStudy/StringMethodExam.java
@@ -0,0 +1,12 @@
+package javaStudy;
+
+
+
+public class StringMethodExam {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println(str.length());
+    }
+
+}
