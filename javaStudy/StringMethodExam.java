f979cd5ebc986dacdc0dc666aaad1eb5e5dd8ad8
sungeunbyeol <87050900+sungeunbyeol@users.noreply.github.com>
2021-07-30 10:49:29 +0900

    Update StringMethodExam.java
    substring 자르기
    (3,6)-> 3번자리부터 6번째자리까지
    (3) -> 3번부터

--- javaStudy/StringMethodExam.java
+++ javaStudy/StringMethodExam.java
@@ -9,6 +9,10 @@ public class StringMethodExam {
         System.out.println(str.length());
         System.out.println(str.concat(" world"));
         System.out.println(str);
+        str = str.concat(" world");
+        System.out.println(str);
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(3,6));
     }
 
 }
