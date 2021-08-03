18759d45334135ecd2185861850173b5ea3ce778
sungeunbyeol <87050900+sungeunbyeol@users.noreply.github.com>
2021-08-03 11:08:07 +0900

    8/3

---
+++ Exception/BizExam.java
@@ -0,0 +1,12 @@
+public class BizExam {
+    public static void main(String[] args) {
+        BizService biz = new BizService();
+        biz.bizMethod(5);
+        try{
+            biz.bizMethod(-5);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("프로그램 종료");
+    }
+}
---
+++ Exception/MycheckedException.java
@@ -0,0 +1,5 @@
+public class MycheckedException extends Exception{
+    public MycheckedException(){
+        super("나의 예외 발생");
+    }
+}
